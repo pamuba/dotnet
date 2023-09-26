712a5e465241eb4697c00a9f014b5900e25e5a13
kavyajadav <kjadhav@stevens.edu>
2023-09-26 20:10:20 +0530

    26 Sep 3

--- ConsoleApp2/ConsoleApp2/Thread1Demo.cs
+++ ConsoleApp2/ConsoleApp2/Thread1Demo.cs
@@ -5,18 +5,21 @@ class MyThread{
     public int Count;
     string thrdName;
 
+    public Thread thrd;
     public MyThread(string name){
         Count = 0;
-        thrdName = name;
+        thrd = new Thread(this.Run);
+        thrd.Name = name;
+        thrd.Start();
     }
 
     //Entry point for the thread
     //containsthe code to be launched as a separate thread
-    public void Run(){
-        Console.WriteLine(thrdName+" starting");
+    void Run(){
+        Console.WriteLine(thrd.Name+" starting");
         do{
             Thread.Sleep(500);
-            Console.WriteLine("In "+thrdName+", Count is "+Count);
+            Console.WriteLine("In "+thrd.Name+", Count is "+Count);
             Count++;
         }
         while(Count<10);
@@ -26,10 +29,19 @@ class MyThread{
 class MultiThread{
     static void Main(){
         Console.WriteLine("Main thread starting");
-        
         MyThread mt = new MyThread("Child 1");
-        Thread newthrd = new Thread(mt.Run);
-        newthrd.Start(); //Main thrd started the child thread
+        MyThread mt1 = new MyThread("Child 2");
+        MyThread mt2 = new MyThread("Child 3");
+
+        
+        mt.thrd.Join();
+        mt1.thrd.Join();
+        mt2.thrd.Join();
+
+
+        Console.WriteLine($"Child 1: {mt.thrd.IsAlive.ToString()}");
+        Console.WriteLine($"Child 2: {mt1.thrd.IsAlive.ToString()}");
+        Console.WriteLine($"Child 3: {mt2.thrd.IsAlive.ToString()}");
 
         Console.WriteLine("Main thread Ending");
     }
