f9e9c24d5d779e8b0b30a4217e0fc040e4ea2fc4
kavyajadav <kjadhav@stevens.edu>
2023-09-26 21:12:15 +0530

    23 sep 6

--- ConsoleApp2/ConsoleApp2/Thread1Demo.cs
+++ ConsoleApp2/ConsoleApp2/Thread1Demo.cs
@@ -1,48 +1,58 @@
-using System;
-using System.Threading;
+// using System;
+// using System.Threading;
 
-class MyThread{
-    public int Count;
-    string thrdName;
+// class MyThread{
+//     public int Count;
+//     string thrdName;
 
-    public Thread thrd;
-    public MyThread(string name, int num){
-        Count = 0;
-        thrd = new Thread(this.Run);
-        thrd.Name = name;
-        thrd.Start(num);
-    }
+//     public Thread thrd;
+//     public MyThread(string name, int num){
+//         Count = 0;
+//         thrd = new Thread(this.Run);
+//         thrd.Name = name;
+//         thrd.Start(num);
+//     }
 
-    //Entry point for the thread
-    //containsthe code to be launched as a separate thread
-    void Run(object num){
-        Console.WriteLine(thrd.Name+" starting");
-        do{
-            Thread.Sleep(500);
-            Console.WriteLine("In "+thrd.Name+", Count is "+Count+" Passed: "+num);
-            Count++;
-        }
-        while(Count<10);
-    }
-}
+//     //Entry point for the thread
+//     //containsthe code to be launched as a separate thread
+//     void Run(object num){
+//         Console.WriteLine(thrd.Name+" starting");
+//         do{
+//             Thread.Sleep(500);
+//             Console.WriteLine("In "+thrd.Name+", Count is "+Count+" Passed: "+num);
+//             Count++;
+//         }
+//         while(Count<10);
+//     }
+// }
 
-class MultiThread{
-    static void Main(){
-        Console.WriteLine("Main thread starting");
-        MyThread mt = new MyThread("Child 1", 5);
-        MyThread mt1 = new MyThread("Child 2", 4);
-        MyThread mt2 = new MyThread("Child 3", 3);
-
-
-        mt.thrd.Join();
-        mt1.thrd.Join();
-        mt2.thrd.Join();
+// class MultiThread{
+//     static void Main(){
+//         Console.WriteLine("Main thread starting");
+//         MyThread mt = new MyThread("Child 1", 5);
+//         MyThread mt1 = new MyThread("Child 2", 4);
+//         MyThread mt2 = new MyThread("Child 3", 3);
 
 
-        Console.WriteLine($"Child 1: {mt.thrd.IsAlive.ToString()}");
-        Console.WriteLine($"Child 2: {mt1.thrd.IsAlive.ToString()}");
-        Console.WriteLine($"Child 3: {mt2.thrd.IsAlive.ToString()}");
+//         mt.thrd.Priority = ThreadPriority.Lowest;
+//         mt1.thrd.Priority = ThreadPriority.Lowest;
+//         mt2.thrd.Priority = ThreadPriority.Highest;
 
-        Console.WriteLine("Main thread Ending");
-    }
-}
+
+//         Console.WriteLine(mt.thrd.Priority);
+//         Console.WriteLine(mt1.thrd.Priority);
+//         Console.WriteLine(mt2.thrd.Priority);
+
+//         mt.thrd.Join();
+//         mt1.thrd.Join();
+//         mt2.thrd.Join();
+
+
+//         Console.WriteLine($"Child 1: {mt.thrd.IsAlive.ToString()}");
+//         Console.WriteLine($"Child 2: {mt1.thrd.IsAlive.ToString()}");
+//         Console.WriteLine($"Child 3: {mt2.thrd.IsAlive.ToString()}");
+
+//         Console.WriteLine(Thread.CurrentThread.Priority);
+//         Console.WriteLine("Main thread Ending");
+//     }
+// }
---
+++ ConsoleApp2/ConsoleApp2/lockDemo.cs
@@ -0,0 +1,49 @@
+class ArraySun{
+    int sum;
+    public int Sum(int[] nums){
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            Console.WriteLine($"Totla from {Thread.CurrentThread.Name} is {sum}");
+            Thread.Sleep(10);
+        }
+        return sum;
+    }
+}
+
+class MyThread{
+    public int[] a;
+    public Thread thrd;
+    int result;
+
+    static object lk = new object();
+
+    public static ArraySun sumArray;
+    static ArraySun GetObj(){
+       return sumArray = new ArraySun();
+    }
+
+    public MyThread(string name, int[] nums){
+        thrd = new Thread(this.Run);
+        thrd.Name = name;
+        thrd.Start();
+    }
+    public void Run(){
+        Console.WriteLine(thrd.Name+" starting");
+        lock(lk) result = MyThread.GetObj().Sum(a);
+        Console.WriteLine(thrd.Name+  "terminating");
+    }
+}
+
+class Sync{
+    static void Main(){
+        int[] a = {1,2,3,4,5};
+
+        MyThread mt1 = new MyThread("Child 1", a);
+        MyThread mt2 = new MyThread("Child 2", a);
+
+        mt1.thrd.Join();
+        mt2.thrd.Join();
+
+    }
+}
