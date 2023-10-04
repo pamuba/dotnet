6ba99b0942e1faf4530f741d8812f35e4afe9215
kavyajadav <kjadhav@stevens.edu>
2023-10-04 20:54:34 +0530

    4 oct

--- ConsoleApp2/ConsoleApp2/Async2.cs
+++ ConsoleApp2/ConsoleApp2/Async2.cs
@@ -9,30 +9,63 @@ namespace ConsoleApp2
     internal class Async2
     {
         //UI
-        static void Main() {
-            Console.WriteLine("Main method Started....");
-            SomeMethod();
-            //SomeMethod();
-            Console.WriteLine("Main method End");
-            Console.ReadLine();
-        }
+        //static void Main() {
+        //    Console.WriteLine("Main method Started....");
+        //    SomeMethod();
+        //    //SomeMethod();
+        //    Console.WriteLine("Main method End");
+        //    Console.ReadLine();
+        //}
 
-        public async static void SomeMethod() {
-            Console.WriteLine("Some method startred");
-            await Wait();
-            //Console.WriteLine();
-            Console.WriteLine("Some Method ended");
-        }
-        //async code synchronously
-        private static async Task Wait()
+        //public async static void SomeMethod() {
+        //    Console.WriteLine("Some method startred");
+        //    await Wait();
+        //    //Console.WriteLine();
+        //    Console.WriteLine("Some Method ended");
+        //}
+        ////async code synchronously
+        //private static async Task Wait()
+        //{
+        //    await Task.Delay(5000); //fetching data
+        //    //returning the data 
+
+        //    await Task.Delay(5000); //fetching data
+        //    //returning the data 
+
+        //    Console.WriteLine("5 seconds wait Completed");
+        //}
+
+
+        public static void Main(string[] args)
         {
-            await Task.Delay(5000); //fetching data
-            //returning the data 
+            Console.WriteLine("Main Method Started......");
+            Console.WriteLine("Enter the Name: ");
+            string Name = Console.ReadLine();
+            SomeMethod(Name);
+            Console.WriteLine("Main Method End");
+            Console.ReadKey();
+        }
+        public async static void SomeMethod(string Name)
+        {
+            Console.WriteLine("Some Method Started......");
+            var GreetingSMessage = await Greetings(Name);
+            Console.WriteLine($"\n{GreetingSMessage}");
+            Console.WriteLine("Some Method End");
+        }
 
-            await Task.Delay(5000); //fetching data
-            //returning the data 
+        public static async Task<string> Greetings(string Name)
+        {
+            string message = string.Empty;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7029/WeatherForecast/");
 
-            Console.WriteLine("5 seconds wait Completed");
+                HttpResponseMessage response = await client.GetAsync($"api/greetings/{Name}");
+                //response.EnsureSuccessStatusCode();
+                message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+            return message;
         }
 
     }
