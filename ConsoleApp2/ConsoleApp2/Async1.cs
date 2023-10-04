7dd11a99a211cf9ee4be242688cf484cd0e65565
kavyajadav <kjadhav@stevens.edu>
2023-10-04 12:43:14 +0530

    Oct1

---
+++ ConsoleApp2/ConsoleApp2/Async1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Async1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started......");
+
+            SomeMethod();
+
+            Console.WriteLine("Main Method End");
+            Console.ReadKey();
+        }
+
+        public static void SomeMethod()
+        {
+            Console.WriteLine("Some Method Started......");
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("\n");
+            Console.WriteLine("Some Method End");
+        }
+    }
+}
