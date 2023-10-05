88412738954dc37c682fae439ee999d23ff10206
kavyajadav <kjadhav@stevens.edu>
2023-10-05 20:49:33 +0530

    5th Oct

---
+++ ConsoleApp2/ConsoleApp2/HashSetDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class HashSetDemo
+    {
+        //unordered
+        //unique
+
+        static void Main() {
+            HashSet<string> hs = new HashSet<string>();
+            hs.Add("Sunday");
+            hs.Add("Monday");
+            hs.Add("Tuesday"); 
+            hs.Add("Monday");
+
+
+            HashSet<string> hs1 = new HashSet<string>();
+            hs1.Add("Sunday");
+            hs1.Add("Friday");
+            hs1.Add("Saturday");
+            hs.Add("Wednesday");
+
+            /*hs.Intersect<string>(hs1)*/;
+
+
+            foreach (var item in hs.Intersect<string>(hs1))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+       
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Task.WhenAllDemo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class TaskDemo
+    {
+        static void Main(){
+            Console.WriteLine("main thread started");
+            List<CreditCard> creidCards = CreditCard.GenerateCreditCards(10);
+
+            ProcessCreditCards(creidCards);
+            
+            Console.WriteLine("main thread ended");
+            Console.ReadLine();
+        }
+        public static async void ProcessCreditCards(List<CreditCard> creditCards) {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var tasks = new List<Task<string>>();
+
+
+            //await Task.Run
+            //Processing
+            foreach (var creditCard in creditCards)
+            {
+                var response = ProcessCard(creditCard);
+                tasks.Add(response);
+            }
+
+            //execute all tasks concurrenlty
+            await Task.WhenAll(tasks);
+
+            //foreach (var creditCard in creditCards) {
+            //    var response = await ProcessCard(creditCard);
+            //    Console.WriteLine(response);
+            //}
+
+            stopWatch.Stop();
+            Console.WriteLine($"Processing of {creditCards.Count} Credit Cards" +
+                $"Done in {stopWatch.ElapsedMilliseconds/1000} Seconds");
+        }
+
+        public static async Task<string> ProcessCard(CreditCard creditCard) { 
+            await Task.Delay(1000);
+            Console.WriteLine($"Credit Card Number:{creditCard.CardNumber} Name: {creditCard.Name} Processed");
+
+            return $"Credit Card Number:{creditCard.CardNumber} Name: {creditCard.Name} Processed";
+        }
+    }
+    //so its ok to not have async if we dont use await
+    public class CreditCard { 
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+
+        public static List<CreditCard> GenerateCreditCards(int number) { 
+            List<CreditCard> creditCards = new List<CreditCard>();
+            for (int i = 0; i < number; i++)
+            {
+                CreditCard card = new CreditCard()
+                {
+                    CardNumber = "1001" + i,
+                    Name = "CreditCard-" + i
+                };
+                creditCards.Add(card);
+            }
+            return creditCards;
+        }
+
+    }
+}
